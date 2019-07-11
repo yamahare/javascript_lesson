c8cf5b3f27b7ef58e0b6a7e36442e537960e7ddf
yamahare <jidai.norio@gmail.com>
2019-07-11 19:55:18 +0900

    [add]webpack.config.js追加

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const env = process.env.NODE_ENV
+const entry = require('webpack-glob-entry');
+
+module.exports = {
+  mode: env || 'development',
+  entry: entry("./src/js/*.js"),
+  output: {
+    path: path.resolve(__dirname,  './public/js/'),
+    filename: '[name].js',
+  },
+  // 拡張子が.jsのファイルはbabel-loaderを通してビルド(node_modulesは除外)
+  module: {
+    rules: [
+      // babel
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  },
+}
+
