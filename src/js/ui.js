38163e16c29cd0096be8320ff5c9de0f17c865e2
yamahare <jidai.norio@gmail.com>
2019-07-14 15:15:20 +0900

    [add]スライドナビゲーションの後ろにoverlay追加した

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', e=>{
   const sidenav = document.getElementsByClassName('sidenav')[0];
+  const overlay = document.getElementById('closeOverlay');
   const openBtn = document.getElementById('openBtn');
   const closeBtn = document.getElementsByClassName('sidenav__close')[0];
 
   openBtn.addEventListener('click', e => {
     sidenav.classList.add('active');
+    overlay.style.display="block";
   })
-  closeBtn.addEventListener('click', e => {
+  function closeSidenav(){
     sidenav.classList.remove('active');
-  })
+    overlay.style.display="none";
+  }
+  closeBtn.addEventListener('click', closeSidenav);
+  overlay.addEventListener('click', closeSidenav);
 })
